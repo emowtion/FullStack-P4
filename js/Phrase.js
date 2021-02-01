d689f2f9dd374c9b28505ab3d8e903a8c2e0232d
Emowtion <emowtion@hotmail.com>
2021-02-02 00:42:21 +0100

    more comments

--- js/Phrase.js
+++ js/Phrase.js
@@ -8,15 +8,17 @@ class Phrase {
 	}
 
 	addPhraseToDisplay() {
+		// loop over the phase sting and create an Li element for each letter
 		const ul = document.querySelector("#phrase ul");
 		for (let i = 0; i < this.phrase.length; i++) {
 			const listItems = document.createElement("li");
 			const liContent = document.createTextNode(this.phrase[i]);
 			listItems.appendChild(liContent);
 			if (listItems.textContent === " ") {
+				// if the textcontent is empty we add a space
 				listItems.classList.add("space");
 			} else {
-				listItems.classList.add("letter");
+				listItems.classList.add("letter"); // if not we add the class letter and hide (grey box)
 				listItems.classList.add("hide");
 			}
 			ul.appendChild(listItems);
@@ -25,6 +27,7 @@ class Phrase {
 
 	checkLetter(letter) {
 		if (this.phrase.includes(letter)) {
+			// check whether the letter is included in the phrase
 			return true;
 		} else {
 			return false;
@@ -32,6 +35,7 @@ class Phrase {
 	}
 
 	showMatchedLetter(letter) {
+		// loop over all letters if their innHTML is equal to our selected letter we remove the grey box and show it in the board
 		let letterClass = document.querySelectorAll(".letter");
 		for (let i = 0; i < letterClass.length; i++) {
 			if (letterClass[i].innerHTML === letter) {
